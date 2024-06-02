f9a045885181d16802cd1143728d294e3793447c
Lu-Han Wang <r10521523@ntu.edu.tw>
2024-06-02 20:31:07 +0800

    feat: complete homedown area

--- client/src/HomeDown.jsx
+++ client/src/HomeDown.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
+import { Text } from "@chakra-ui/react";
 import "./HomeDown.css";
 import Movie from "./Movie";
 
 const isSearch = false;
+const MOVIE_PER_ROW = 3;
 
 function HomeDown() {
   const [movies, setMovies] = useState([]);
@@ -10,21 +12,38 @@ function HomeDown() {
   useEffect(() => {
     fetch("http://localhost:3000/api/recommend/movie?id=929590")
       .then((response) => response.json())
-      .then((json) => [json[0]])
       .then((json) => setMovies(json));
   }, []);
 
   return (
     <>
-      <h2>{isSearch ? "搜尋結果：" : "熱門推薦："}</h2>
+      <Text
+        mt={2}
+        fontSize="xl"
+        fontWeight="semibold"
+        lineHeight="short"
+        className="mainWord"
+      >
+        {isSearch ? "搜尋結果：" : "熱門推薦："}
+      </Text>
       <div className="row">
-        {movies.map((movie) => (
+        {movies.slice(0, MOVIE_PER_ROW).map((movie) => (
           <Movie movie={movie._source} />
           // <p>{movie._source.original_title} </p>
         ))}
       </div>
-      <div className="row">Row2</div>
-      <div className="row">Row3</div>
+      <div className="row">
+        {movies.slice(MOVIE_PER_ROW, MOVIE_PER_ROW * 2).map((movie) => (
+          <Movie movie={movie._source} />
+          // <p>{movie._source.original_title} </p>
+        ))}
+      </div>
+      <div className="row">
+        {movies.slice(MOVIE_PER_ROW * 2, MOVIE_PER_ROW * 3).map((movie) => (
+          <Movie movie={movie._source} />
+          // <p>{movie._source.original_title} </p>
+        ))}
+      </div>
     </>
   );
 }
--- client/src/Movie.jsx
+++ client/src/Movie.jsx
@@ -1,30 +1,29 @@
-import { Box, Center, Image, Flex, Badge, Text } from "@chakra-ui/react";
-import { MdStar } from "react-icons/md";
+import { Box, Center, Image, Flex, Text } from "@chakra-ui/react";
+import { MdStar, MdWhatshot, MdDateRange } from "react-icons/md";
 
 function Movie({ movie }) {
   return (
-    <Center h="100vh">
-      <Box p="5" maxW="320px" borderWidth="1px">
+    <Center h="55vh">
+      <Box p="5" maxW="380px" borderWidth="2px" borderRadius={20}>
         <Image
           borderRadius="md"
-          src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2/${movie.poster_path}`}
+          src={`https://image.tmdb.org/t/p/w355_and_h200_multi_faces/${movie.backdrop_path}`}
         />
-        {/* <Flex align="baseline" mt={2}>
-          <Badge colorScheme="pink">Plus</Badge>
-          <Text
-            ml={2}
-            textTransform="uppercase"
-            fontSize="sm"
-            fontWeight="bold"
-            color="pink.800"
-          >
-            Verified &bull; Cape Town
-          </Text>
-        </Flex> */}
         <Text mt={2} fontSize="xl" fontWeight="semibold" lineHeight="short">
           {movie.original_title}
         </Text>
-        <Text mt={2}>$119/night</Text>
+        <Flex mt={2} align="center">
+          <Box as={MdDateRange} color="green.400" />
+          <Text ml={1} fontSize="sm">
+            <i>{movie.release_date}</i>
+          </Text>
+        </Flex>
+        <Flex mt={2} align="center">
+          <Box as={MdWhatshot} color="red.400" />
+          <Text ml={1} fontSize="sm">
+            <b>{Math.floor(movie.popularity)}</b>
+          </Text>
+        </Flex>
         <Flex mt={2} align="center">
           <Box as={MdStar} color="orange.400" />
           <Text ml={1} fontSize="sm">
