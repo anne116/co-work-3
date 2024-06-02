50cfb4922d1fc4674b8c64f1f5b41a4490a41720
e055339 <e055339@gmail.com>
2024-06-02 17:41:45 +0800

    Style: change file name and refactor files

--- server/models/searchMovies.js
+++ server/models/searchMovies.js
@@ -30,3 +30,31 @@ export const searchMovieByKeyword = async (keyword) => {
   const data = await response.json();
   return data.hits.hits;
 };
+
+export const getMovieById = async (id) => {
+  const searchQuery = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: {
+        match: {
+          _id: id,
+        },
+      },
+    }),
+  };
+
+  const response = await fetch(
+    `http://${process.env.esHost}/movies/_search`,
+    searchQuery
+  );
+
+  if (!response.ok) {
+    throw new Error(`Error: ${response.statusText}`);
+  }
+
+  const data = await response.json();
+  return data.hits.hits;
+};
--- server/routes/movieDetail.js
+++ server/routes/movieDetail.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { searchMovies } from "../controllers/searchMovies.js";
+import { movieDetail } from "../controllers/movieDetail.js";
 import asyncWrapper from "../middlewares/asyncWrapper.js";
 
 const router = Router();
 
-router.route("/movies").get(asyncWrapper(searchMovies));
+router.route("/").get(asyncWrapper(movieDetail));
 
 export default router;
